6f7a8c1d2ce04b8f3b01413f6459a8c102256523
Denis Gorodetskiy <noufos@gmail.com>
2011-01-21 08:21:13 +0900

    first commit

---
+++ binary-input/pricer_types.h
@@ -0,0 +1,20 @@
+#ifndef __PRICER_TYPES_H_1203912039843993059646023480__
+#define __PRICER_TYPES_H_1203912039843993059646023480__
+
+#include <stdint.h>
+
+typedef uint64_t order_id_t;
+typedef uint32_t timestamp_t;
+typedef double    price_t;
+
+struct message_t
+{
+    int             timestamp;
+    char            msg_type;   // 'A' for add or 'R' for reduce
+    uint64_t		id;			// order id
+    int             size;       // number of shares
+	char			side;		// 'B' or 'S'
+	double			price;
+};
+
+#endif
---
+++ text-input/pricer_types.h
@@ -0,0 +1,10 @@
+#ifndef __PRICER_TYPES_H_1203912039843993059646023480__
+#define __PRICER_TYPES_H_1203912039843993059646023480__
+
+#include <stdint.h>
+
+typedef uint64_t order_id_t;
+typedef uint32_t timestamp_t;
+typedef double   price_t;
+
+#endif
